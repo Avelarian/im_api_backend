5b03cc636500739bc69088077c3e8339f0eb7268
Jean de Dieu TWAGIRUMUHOZA <jdd.twagirumuhoza@webpak.io>
2021-05-04 11:46:48 +0200

    created controllers directory and implemented doctor-controller.js, added functions to get all Doctors and one Doctor endpoints, i also added routes for these endpoint in routes.js

---
+++ controllers/doctor-controller.js
@@ -0,0 +1,48 @@
+
+const doctor = require('../models/Doctor')
+
+
+module.exports = {
+    // List all doctors.
+    async list(request, response) {
+        const doctors = await Doctor.findAll()
+        .catch((error) => {
+            return res.status(500).json({
+                error_point: 'Doctors list.',
+                message: 'Error trying to list  doctors.',
+                error: error,
+            });
+        });
+        
+        return res.status(200).json({
+            message: 'Doctors listed!',
+            list_doctors: doctors,
+        });
+    },
+
+      // Get one Doctor.
+      async index(request, response) {
+        const {id} = request.params;
+
+        const doctor = await Doctor.findByPk(id)
+        .catch((error) => {
+            return res.status(500).json({
+                error_point: 'Doctor index.',
+                message: 'Error trying to get a doctor.',
+                error: error,
+            });
+        }); // Error for accesing the database.
+
+        if (!doctor) {
+            return res.status(400).json({
+                error_point: 'Doctor index.',
+                message: 'Doctor not found.'
+            });
+        } // Checking if Doctor exists.
+        
+        return res.status(200).json({
+            message: 'Doctor found!',
+            doct_point: doctor,
+        });
+    },
+}
