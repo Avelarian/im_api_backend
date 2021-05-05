c1a38ac4be7665dd4730e217c58f903ddc70934a
Ian Avelar Peixoto <avelarian@gmail.com>
2021-05-05 19:23:25 +0200

    :wrench: Updated config files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require("cors");
 
 const routes = require("./routes");
+const connection = require("./database");
 
 const app = express()
 const port = 3333
@@ -9,6 +10,8 @@ const port = 3333
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+connection.sync();
 
 app.listen(port, () => {
     console.log(`Service is available on port ${port}`)
